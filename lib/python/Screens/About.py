9b78fd15e428ed933d3085ac8bb0d8974e2d5efe
Open-cobralibero <121136157+open-cobralibero@users.noreply.github.com>
2024-12-05 11:13:17 +0100

    Update About.py

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -36,9 +36,11 @@ class About(Screen):
 		cpu = about.getCPUInfoString()
 		AboutText += _("CPU: ") + cpu + "\n"
 		AboutText += _("Image: ") + about.getImageTypeString() + "\n"
-		AboutText += _("OE Version: ") + about.getOEVersionString() + "\n"
-		AboutText += _("Build date: ") + about.getBuildDateString() + "\n"
-		AboutText += _("Last update: ") + about.getUpdateDateString() + "\n"
+		ImageVersion = _("Last update: ") + about.getImageVersionString()
+		self["ImageVersion"] = StaticText(ImageVersion)
+		AboutText += ImageVersion + "\n" 
+		AboutText += _("Build Image: open-cobralibero") + "\n"
+		AboutText += _("Forum: https://cobraliberosat.net") + "\n"
 
 		# [WanWizard] Removed until we find a reliable way to determine the installation date
 		# AboutText += _("Installed: ") + about.getFlashDateString() + "\n"
